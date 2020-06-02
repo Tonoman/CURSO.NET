d05859359ea6f38a0aafb2bba0a09cfb070231a9
Tono Albiol <tono.albiol@gmail.com>
2020-06-02 13:44:02 +0200

    Actualizacion 02-06-2020

---
+++ backendAppAngular/ClientApp/src/app/components/usuario-form-mantenimiento/usuario-form-mantenimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-form-mantenimiento',
+  templateUrl: './usuario-form-mantenimiento.component.html',
+  styleUrls: ['./usuario-form-mantenimiento.component.css']
+})
+export class UsuarioFormMantenimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
